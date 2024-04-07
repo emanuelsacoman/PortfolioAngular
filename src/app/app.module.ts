d7b1dfd3f82f63dbc1575c134261cff3990a227e
Emanuel Vinícius Sacoman <emanuelsacoman@gmail.com>
2024-04-07 15:23:52 -0300

    ~nada especial~

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './view/usuario/index/index.component';
+import { LoginComponent } from './view/usuario/login/login.component';
+import { AdminComponent } from './view/adm/admin/admin.component';
+import { environments } from 'src/environments/environments';
+import { AuthService } from './model/services/auth.service';
+import { AuthGuard } from './shared/auth.guard';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule, } from '@angular/fire/compat/storage';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    LoginComponent,
+    AdminComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environments.firebase)),
+    provideFirestore(() => getFirestore()),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireModule,
+    ReactiveFormsModule,
+    AngularFireAuthModule,
+    provideAuth(() => getAuth()),
+  ],
+  providers: [
+    { provide: FIREBASE_OPTIONS, useValue: environments.firebase }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
